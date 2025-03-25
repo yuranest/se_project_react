d3a857104d06f1843a388db33f18c483e28b3e5a
yuranest <yuranest@gmail.com>
2025-03-25 17:05:34 +0200

    Add working code and updated styles

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,13 +1,18 @@
 import "./ItemModal.css";
+import closeIcon from "../../assets/item-modal__close.png";
 
 function ItemModal({ item, onClose }) {
   return (
     <div className="modal modal_type_preview">
       <div className="modal__content">
-        <button className="modal__close" type="button" onClick={onClose} />
+        <button className="modal__close" type="button" onClick={onClose}>
+          <img src={closeIcon} alt="Close button" />
+        </button>
         <img src={item.link} alt={item.name} className="modal__image" />
-        <p className="modal__caption">{item.name}</p>
-        <p className="modal__weather">Weather: {item.weather}</p>
+        <ul className="modal__info">
+          <li className="modal__caption">{item.name}</li>
+          <li className="modal__weather">Weather: {item.weather}</li>
+        </ul>
       </div>
     </div>
   );
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import "./ModalWithForm.css";
+import closeIcon from "../../assets/form-modal__close.png";
 
 function ModalWithForm({ title, name, buttonText, onClose, children }) {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    imageUrl: "",
+    weather: "hot",
+  });
+
+  const isFormValid = formValues.name && formValues.imageUrl;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className={`modal modal_type_${name}`}>
       <form className="modal__form" name={name}>
-        <button type="button" className="modal__close" onClick={onClose}>
-          ✕
+        <button className="modal__close" type="button" onClick={onClose}>
+          <img src={closeIcon} alt="Close button" />
         </button>
         <h2 className="modal__title">{title}</h2>
 
@@ -17,6 +35,8 @@ function ModalWithForm({ title, name, buttonText, onClose, children }) {
             name="name"
             placeholder="Name"
             required
+            value={formValues.name}
+            onChange={handleChange}
           />
         </label>
 
@@ -28,6 +48,8 @@ function ModalWithForm({ title, name, buttonText, onClose, children }) {
             name="imageUrl"
             placeholder="Image URL"
             required
+            value={formValues.imageUrl}
+            onChange={handleChange}
           />
         </label>
 
@@ -39,7 +61,8 @@ function ModalWithForm({ title, name, buttonText, onClose, children }) {
               name="weather"
               value="hot"
               className="modal__radio"
-              defaultChecked
+              checked={formValues.weather === "hot"}
+              onChange={handleChange}
             />
             Hot
           </label>
@@ -49,6 +72,8 @@ function ModalWithForm({ title, name, buttonText, onClose, children }) {
               name="weather"
               value="warm"
               className="modal__radio"
+              checked={formValues.weather === "warm"}
+              onChange={handleChange}
             />
             Warm
           </label>
@@ -58,6 +83,8 @@ function ModalWithForm({ title, name, buttonText, onClose, children }) {
               name="weather"
               value="cold"
               className="modal__radio"
+              checked={formValues.weather === "cold"}
+              onChange={handleChange}
             />
             Cold
           </label>
@@ -65,7 +92,7 @@ function ModalWithForm({ title, name, buttonText, onClose, children }) {
 
         {children}
 
-        <button type="submit" className="modal__submit">
+        <button type="submit" className="modal__submit" disabled={!isFormValid}>
           {buttonText}
         </button>
       </form>
